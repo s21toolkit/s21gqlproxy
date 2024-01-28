444eccf8a213b7ebef22eebf9b70b546c12338ae
EnergoStalin <ens.stalin@gmail.com>
2024-01-28 21:20:58 +0300

    feat: gql saving

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Конфига для проксирования edu.21-school.ru/services/graphql траффика через Mitmproxy с записью har лога
 
 > :warning: После запуска установите сертификат из certs папки либо после импорта конфиги [foxyproxy](https://getfoxyproxy.org/downloads/) через [mitm.it](http://mitm.it)!!!
+
+Питон нужен только для удобства разработки скриптов
---
+++ config/har.py
@@ -0,0 +1,72 @@
+from mitmproxy import http
+from os import getenv
+from pathlib import Path
+from json import dumps, loads
+import logging
+
+class GQLHarLogEntry:
+	def __init__(self, opName: str, query: str, response: str) -> None:
+		self.query = query
+		self.response = response
+		self.opName = opName
+
+	def save(self, folder: Path) -> None:
+		folder.joinpath(f'{self.opName}.json').write_text(self.__str__())
+
+	def __str__(self):
+		return dumps({
+			'log': {
+				'entries': [{
+					'request': {
+						'cookies': {},
+						'headers': [],
+						'postData': {
+							'text': self.query,
+						}
+					},
+					'response': {
+						'cookies': {},
+						'headers': [],
+						'content': {
+							'text': self.response,
+						}
+					}
+				}]
+			}
+		}, indent=2)
+
+class Extractor:
+	def __init__(self):
+		self.ROOT_DIR = Path(getenv('HOME') or '~', '.mitmproxy', 'dump')
+		self.QUERIES_DIR = Path(self.ROOT_DIR, 'queries')
+		self.MUTATIONS_DIR = Path(self.ROOT_DIR, 'mutations')
+
+		self.QUERIES = set()
+		self.MUTATIONS = set()
+
+		self.ROOT_DIR.mkdir(exist_ok=True)
+		self.QUERIES_DIR.mkdir(exist_ok=True)
+		self.MUTATIONS_DIR.mkdir(exist_ok=True)
+		
+
+	def response(self, flow: http.HTTPFlow):
+		if flow.request.url.endswith('/services/graphql'):
+			if flow.request.content and flow.response and flow.response.content:
+				rawReq = flow.request.content.decode()
+				req = loads(rawReq)
+
+
+				res = flow.response.content.decode()
+				if req['query'].startswith('query') and not req['operationName'] in self.QUERIES:
+					self.QUERIES.add(req['operationName'])
+					GQLHarLogEntry(req['operationName'], req, res).save(self.QUERIES_DIR)
+				elif req['query'].startswith('mutation') and not req['operationName'] in self.MUTATIONS:
+					self.MUTATIONS.add(req['operationName'])
+					GQLHarLogEntry(req['operationName'], req, res).save(self.MUTATIONS_DIR)
+		elif flow.request.url.endswith('/static.js'):
+				if flow.request.raw_content:
+					self.ROOT_DIR.joinpath('static.js').write_bytes(flow.request.raw_content)
+
+
+
+addons = [Extractor()]
